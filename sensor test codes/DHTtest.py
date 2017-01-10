0754199d9fe12e191c48caa56a162a6b401a6734
pothigai <pothigaiselvan@gmail.com>
2017-01-10 06:14:30 +0000

    Add files via upload

---
+++ sensor test codes/DHTtest.py
@@ -0,0 +1,8 @@
+import sys
+import Adafruit_DHT
+humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, 17)
+if humidity is not None and temperature is not None:
+    print('Temp={0:0.1f}*C Humidity={1:0.1f}%'.format(temperature, humidity))
+else:
+    print('Failed to get reading.Try again!')
+    
